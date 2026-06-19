ac90baeb3affcc5dd736c6d397bf7c8426f1ee1a
lstocchi <lstocchi@redhat.com>
2026-06-19 17:30:35 +0200

    virtio/fs: add Windows fs module
    Introduces the windows/ module mirroring what's done for other OSes.
    
    passthrough.rs is just a placeholder to set up the structure and it will
    be updated in following commits.
    
    fs_utils.rs: defines Linux/POSIX constants not provided by the
    Windows libc crate (O_ACCMODE, W_OK, SEEK_DATA, SEEK_HOLE,
    ST_RDONLY), convenience error constructors (ebadf, einval,
    enosys), and win_err_to_linux() which maps Windows raw error
    codes and Rust ErrorKind values to their Linux errno equivalents
    for the FUSE protocol.
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- third_party/libkrun/src/devices/src/virtio/fs/mod.rs
+++ third_party/libkrun/src/devices/src/virtio/fs/mod.rs
@@ -25,6 +25,12 @@ pub mod macos;
 pub use macos::fs_utils;
 #[cfg(target_os = "macos")]
 pub use macos::passthrough;
+#[cfg(target_os = "windows")]
+pub mod windows;
+#[cfg(target_os = "windows")]
+pub use windows::fs_utils;
+#[cfg(target_os = "windows")]
+pub use windows::passthrough;
 
 use super::bindings;
 use super::descriptor_utils;
